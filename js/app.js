a60efe822e8568ae036b46076f08b55900b77dca
michaltruszczynski <michal.truszczynski2@gmail.com>
2019-10-16 19:13:15 +0200

    layout draft

---
+++ js/app.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+
+    //MENU
+    const nav = document.querySelector('nav');
+
+    function fixNav() {
+        if (window.scrollY > 10) {
+            nav.classList.add('menu--white')
+        } else {
+            nav.classList.remove('menu--white')
+        }
+    };
+
+    window.addEventListener('scroll', fixNav);
+
+} );
