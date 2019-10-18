98086cbb4fc412e4c05c24f7973ed6057f42077d
michaltruszczynski <michal.truszczynski2@gmail.com>
2019-10-18 13:46:13 +0200

    layout draft carousel

--- js/app.js
+++ js/app.js
@@ -26,4 +26,35 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     menuBtn.addEventListener('click', menuToggle);
 
+    //CAROUSEL
+
+    const track = document.querySelector('.carousel__track');
+    const slides = Array.from(track.children);
+
+    const nextButton = document.querySelector('.carousel__button--right');
+    const prevButton = document.querySelector('.carousel__button--left');
+
+    const dotsNav = document.querySelector('.carousel__nav');
+    const dots = Array.from(dotsNav.children);
+
+    const slideWidth = slides[0].getBoundingClientRect().width;
+
+    //arrange the slides next to one another
+    slides.forEach((slide, index) => {
+        slide.style.left = slideWidth * index + 'px';
+    })
+
+    // slides[0].style.left = slideWidth * 0 + 'px';
+    // slides[1].style.left = slideWidth * 1 + 'px';
+    // slides[2].style.left = slideWidth * 2 + 'px';
+
+    console.log(slides);
+
+    //click left
+
+    //clisk right
+
+    //slide indicators
+
+
 } );
