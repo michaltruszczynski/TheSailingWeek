5e826212703dfa9ada535ebf619115d2358cabd2
michaltruszczynski <michal.truszczynski2@gmail.com>
2020-03-10 23:23:59 +0100

    layout draft

--- js/app.js
+++ js/app.js
@@ -1,5 +1,48 @@
 document.addEventListener("DOMContentLoaded", function (event) {
 
+    //MENU - bgc on scroll
+    const nav = document.querySelector('nav');
+
+    function fixNav() {
+        if (window.scrollY > 10) {
+            nav.classList.add('menu--white')
+        } else {
+            nav.classList.remove('menu--white')
+        }
+    };
+
+    window.addEventListener('scroll', fixNav);
+
+    //MENU - open/close on click
+
+    const menuBtn = document.querySelector('.menu__toggle');
+    const menu = document.querySelector('nav ul');
+
+    console.log(menuBtn)
+
+    function menuToggle() {
+        menu.classList.toggle('menu--toggle')
+    }
+
+    menuBtn.addEventListener('click', menuToggle);
+
+    class NavBar {
+        constructor(menuSelector) {
+            this.menuSelector = menuSelector;
+            this.menu = null;
+        }
+
+        generateMenu() {
+            //dodatkowo sprawdzenie
+            this.menu = document.querySelector(this.menuSelector);
+            window.addEventListener('scroll', () => {
+
+            });
+        }
+
+    }
+
+
     class Accordions {
         constructor(accordionSelector) {
             this.accordionSelector = accordionSelector;
@@ -24,10 +67,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
             this.optionMenu = null;
         }
 
-
-    }
-
-    class NavBar {
 
     }
 
